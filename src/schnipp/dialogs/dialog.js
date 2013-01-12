918aba5af992f02ad078886bfc685dc993d87ef2
Hendrik Speidel <speidel@schnapptack.de>
2013-01-12 17:06:08 +0100

    multiple dialog improvements: -better component initialization -dialog stacking -component alignment patch by @tonimichel

--- src/schnipp/dialogs/dialog.js
+++ src/schnipp/dialogs/dialog.js
@@ -1,6 +1,9 @@
 schnipp.dialogs.dialog = function(parent, component_name) {
+    
     var self = schnipp.dialogs.component(parent, component_name)
     self.ui_type='dialog'
+    self.dom.content = null
+    self.href = null
     
     self.tmpl.main = _.template('\
         <div data-schnui="dialog" class="schn_dialog">\
@@ -11,13 +14,17 @@ schnipp.dialogs.dialog = function(parent, component_name) {
             <div class="schn_content">\
             </div>\
         </div>')
-        
-    self.dom.content = null
     
+    //self.tmpl.overlay = '<div class="schn_overlay"></div>'
+        
+        
     //set dialog content
-    self.view.set_content = function(content) {
+    self.view.set_content = function(content, href) {
+        self.href = href || null
         self.dom.content.html(content)
         self.ctrl.init_content()
+        // set the width of the draggable dialog to prevent glue effect when right:0
+        //self.dom.view.css('width', self.dom.content.width() + 'px')
         return self
     }
     
@@ -30,7 +37,11 @@ schnipp.dialogs.dialog = function(parent, component_name) {
     //set draggability
     self.view.draggable = function(draggable) {
         if (draggable) {
-            self.dom.view.draggable({ stack: ".schn_dialog" })
+            self.dom.view.draggable({
+                start: schnipp.dialogs.stack.push_to_front,
+                handle: '.schn_titlebar'
+            }).click(schnipp.dialogs.stack.push_to_front)
+                .trigger('click')
         } else {
             self.dom.view.draggable('destroy')
         }
@@ -47,6 +58,22 @@ schnipp.dialogs.dialog = function(parent, component_name) {
         return self
     }
     
+    self.ctrl.load = function(href) {
+        schnipp.net.get(
+            href,
+            function(data) {
+                self.view.set_content($(data), href)
+            }
+        )
+    }
+    
+    self.ctrl.reload = function() {
+        if (self.href != null)
+            self.ctrl.load(self.href)
+        else
+            ;//console.log('unable to reload dialog ... self.href is null')
+    }
+    
     //cleanup
     self.ctrl.do_cleanup = function() {
         self.dom.view.remove()
@@ -57,16 +84,70 @@ schnipp.dialogs.dialog = function(parent, component_name) {
     
     //initialize dialog view
     self.ctrl.init_view = function() {
+        // store the initial scrollTop. This is especially important with
+        // huge dialogs that reload themeselves. E.g. consider a
+        // huge form, where the save button is on the bottom. To click this
+        // button, the user must scroll down. When the dialog is re-rendered,
+        // and the center method would pick the current scroll top position, the dialog's
+        // top property would be that huge scrollTop. SO, WE STORE THE
+        // SCROLLTOP WHEN THE DIALOG IS OPENED THE FIRST TIME. This value
+        // is further used by the center method.
+        self.initial_scroll_top = $(document).scrollTop() 
+        
         self.dom.view.find('.schn_close').click(function() {
             self.ctrl.close()
         })
+        
+        // center dialog on resize
+        $(window).resize(function() {
+            self.center_dialog()
+        })
+        
         self.view.draggable(true)
         return self
     }
     
+ 
+    // center the dialog   
+    self.center_dialog = function() {
+        /*var left = ($(document).width() - self.dom.view.width()) / 2
+        var y_delta = $(window).height() - self.dom.view.height() 
+        
+        if (y_delta >= 0)
+            var top = (y_delta) / 2
+        else
+            var top = 0
+        
+        
+        if (self.dom.view.height() < $(window).height()) 
+            top += $(document).scrollTop()
+        //top += window.pageYOffset / 2
+            
+        self.dom.view.css({
+            'top': top + 'px',
+            'left': left + 'px'
+        })*/
+        
+        var left = ($(document).width() - self.dom.view.width()) / 2
+        var y_delta = $(window).height() - self.dom.view.outerHeight() 
+        
+        if ($(window).height() >= self.dom.view.height())
+            var top = (y_delta / 2) + $(document).scrollTop() 
+        else
+            var top = self.initial_scroll_top
+
+        self.dom.view.css({
+            'top': top + 'px',
+            'left': left + 'px'
+        })
+    }
+    
+    
+    
     //initialize dialog content
     self.ctrl.init_content = function() {
         //set title
+        self.center_dialog()
         var title = ''
         var title_elem = self.dom.content.find(
             schnipp.dialogs._conf.selectors.dialog_title
@@ -80,6 +161,7 @@ schnipp.dialogs.dialog = function(parent, component_name) {
         
         schnipp.dialogs.enable_for(self.dom.content, self)
         schnipp.dialogs._bootstrapper.fire_and_unbind('bootstrap', self)
+        
         return self
     }
     
--- src/schnipp/dialogs/tabber.js
+++ src/schnipp/dialogs/tabber.js
@@ -1,7 +1,8 @@
-
 schnipp.dialogs.tabber = function(parent, component_name) {
     var self = schnipp.dialogs.component(parent, component_name)
-    self.ui_type='tabber'
+    self.toplevel_ajax_component = false
+    self.current_tabhead = null
+    self.ui_type = 'tabber'
     
     self.tmpl.main = _.template('\
         <div data-schnui="tabber" class="schn_tabs">\
@@ -9,29 +10,54 @@ schnipp.dialogs.tabber = function(parent, component_name) {
             </div>\
             <div class="schn_tabcontent">\
             </div>\
-            <div class="schn_tabtmpls" style="display:none">\
-            </div>\
         </div>')
         
     self.dom.content = null
     
     //set dialog content
     self.view.set_content = function(content) {
-        self.dom.content.html(content)
-        self.ctrl.init_content()
+        self.toplevel_ajax_component = true
+        self.dom.remote.html(content)
+        self.view._activate_tab(self.dom.remote)
         return self
     }
     
     self.view._select = function(elem) {
         self.dom.tabbar.find('a').removeClass('current')
         elem.addClass('current')
+        self.current_tabhead = elem
+    }
+    
+    self.view._activate_tab = function(elem) {
+        self.dom.tabcontent.children().hide()
+        self.dom.content = elem
+        elem.show()
+        if (elem.hasClass('remote'))
+            elem.data('schn-inited', false)
+        if (!elem.data('schn-inited')) {
+            self.ctrl.init_content()
+            elem.data('schn-inited', true)
+        }
+    }
+    
+    self.view.activate_tab = function(index) {
+        var links = self.dom.tabbar.find('a')
+        var thelink = $(links[index])
+        thelink.click()
+    }
+    
+    self.ctrl.reload = function() {
+        if (self.current_tabhead != null)
+            self.current_tabhead.click()
+        else
+            ;//console.log('unable to reload dialog ... self.href is null')
     }
     
     self.ctrl._loadcontent = function(tabhead_elem) {
         var inline_tab = tabhead_elem.attr('data-inlinetab')
         if (inline_tab) {
-            var inline_tabcontent = self.dom.tabtmpls.find('[data-inlinetab="' + inline_tab + '"]')
-            self.view.set_content(inline_tabcontent.html())
+            var inline_tabcontent = self.dom.tabcontent.find('[data-inlinetab="' + inline_tab + '"]')
+            self.view._activate_tab(inline_tabcontent)
             return
         }
         var remote_tab = tabhead_elem.attr('data-remotetab')
@@ -39,11 +65,22 @@ schnipp.dialogs.tabber = function(parent, component_name) {
             schnipp.net.get(
                 remote_tab,
                 function(data) {
-                    self.view.set_content($(data))
+                    var content = $(data)
+                    self.view.set_content(content)
+                    //$(document).trigger('dom-changed')
                 }
             )
             return
         }
+    }
+    
+    self.ctrl.add_inlinetab = function(key, title, content) {
+        var tablink = $('<a data-inlinetab="' + key + '">' + title + '</a>')
+        self.dom.tabbar.append(tablink)
+        var tabcontent = $('<div data-inlinetab="' + key + '"></div>')
+        self.dom.tabcontent.append(tabcontent)
+        tabcontent.append(content)
+        self.ctrl.init_view()
     }
     
     //cleanup
@@ -62,30 +99,36 @@ schnipp.dialogs.tabber = function(parent, component_name) {
     
     //initialize dialog view
     self.ctrl.init_view = function() {
-        self.dom.tabbar.find('a').click(self._handlers.tabselect)
+        self.dom.tabbar.find('a').unbind('click').click(self._handlers.tabselect)
         return self
     }
     
+    //register elems
     self.do_register_elems = function() {
         self.dom.tabbar = self.dom.view.children('.schn_tabbar')
-        self.dom.content = self.dom.view.children('.schn_tabcontent')
-        self.dom.tabtmpls = self.dom.view.children('.schn_tabtmpls')
+        self.dom.tabcontent = self.dom.view.children('.schn_tabcontent')
+        self.dom.remote = self.dom.tabcontent.children('.remote')
+        if (!self.dom.remote.length) {
+            self.dom.remote = $('<div class="remote"/>')
+            self.dom.tabcontent.append(self.dom.remote)
+        }
+        self.view._activate_tab(self.dom.tabcontent.children(':first'))
+        self.scope = self.dom.view.attr('data-schnui-scope') || null;
     }
     
     //initialize dialog content
     self.ctrl.init_content = function() {
-        console.log('init_content', self, self.dom.content)
         schnipp.dialogs.enable_for(self.dom.content, self)
-        schnipp.dialogs._bootstrapper.fire_and_unbind('bootstrap', self)
+        if (self.toplevel_ajax_component) {
+            schnipp.dialogs._bootstrapper.fire_and_unbind('bootstrap', self)
+            self.toplevel_ajax_component = false
+        }
         return self
     }
     
     //display the dialog ... normally called just once
     self.do_render = function() {
-        var view = $(self.tmpl.main({
-            title: ''
-        }))
-        self.dom.content = view.find('.schn_content')
+        var view = $(self.tmpl.main())
         return view
     }
     
