ffa17113c50517adaff4a49a9a337140e516b6f3
Hendrik Speidel <speidel@schnapptack.de>
2013-01-12 17:34:02 +0100

    added spinner support (places busy indicator in overlay to temporarily block the user from interacting with the page)

--- src/schnipp.js
+++ src/schnipp.js
@@ -1,1 +1,16 @@
 var schnipp = {};
+
+schnipp.spinner = {}
+
+schnipp.spinner.show = function() {
+    var dom_elem = $('#spinner')
+    if (!dom_elem.length) {
+        dom_elem = $('<div id="spinner"><img src="/static/schnipp/ajax_loader.gif"></div>')
+        $(document.body).append(dom_elem)
+    }
+    dom_elem.show()
+}
+
+schnipp.spinner.hide = function() {
+    $('#spinner').hide()
+}
