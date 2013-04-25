3df9811da9c3fdfd4cac9482f71a07dfc65fd2e0
Toni Michel <michel@schnapptack.de>
2013-04-25 19:54:03 +0200

    enhanced dialog

--- src/schnipp/ui/dialog.js
+++ src/schnipp/ui/dialog.js
@@ -1,23 +1,27 @@
 
-schnipp.ui.dialog = function() {
+schnipp.ui.Dialog = function() {
 
     var self = {}
+    self.events = schnipp.events.event_support()
     self.dom = {}
     self.dom.main = $('\
         <div class="schnipp-dialog">\
-            <a class="schnipp-dialog-close">close</a>\
-            <div class="schnipp-dialog-header"></div>\
-            <div>class="schnipp-dialog-content"</div>\
+            <div class="schnipp-dialog-inner">\
+                <a class="schnipp-dialog-close">close</a>\
+                <div class="schnipp-dialog-header"></div>\
+                <div class="schnipp-dialog-content"</div>\
+            </div>\
         </div>\
     ')
-    self.dom.content = self.dom.main.find('.dialog-content')
-    self.dom.header = self.dom.main.find('.dialog-header')
-    self.dom.close = self.dom.main.find('.dialog-close')
+    self.dom.content = self.dom.main.find('.schnipp-dialog-content')
+    self.dom.header = self.dom.main.find('.schnipp-dialog-header')
+    self.dom.close = self.dom.main.find('.schnipp-dialog-close')
 
     
     self.init = function() {
         self.dom.close.click(self.close)
-        self.dom.main.click(self.top_dialog)
+        self.dom.main.click(self.focus)
+        $('body').append(self.dom.main)
         return self
     }
     
@@ -25,30 +29,47 @@ schnipp.ui.dialog = function() {
         self.dom.content.empty()
         self.dom.content.append(content)
         self.center()
+        self.events.fire('content-change', {
+            evt: 'content-change',
+            dialog: self,
+            content: content
+        })
     }  
       
     
     self.set_title = function(title) {
         self.dom.header.empty()
+        self.dom.header.addClass('dialog-has-title')
         self.dom.header.append(title)
+        self.events.fire('title-change', {
+            evt: 'title-change',
+            dialog: self,
+            content: title
+        })
     }  
     
     
-    self.open = function() {
-        self.events.trigger('open')
+    
+    
+    self.show = function() {
+        self.events.trigger('show')
         self.dom.main.show()
+        self.events.fire('show', {
+            evt: 'show',
+            dialog: self
+        })
     }
     
     
     self.close = function() {
         self.events.trigger('close')
-        self.dom.main.hide()
+        self.dom.main.remove()
+        self.events.fire('close', {
+            evt: 'close',
+            dialog: self
+        })
     }
     
-    self.destroy = funtion() {
-        self.events.trigger('destroy')
-        self.dom.main.remove()
-    }
     
     // ----------------
     
@@ -61,13 +82,13 @@ schnipp.ui.dialog = function() {
         var window_width = $(window).width()
         var window_height = $(window).height()
         
-        var new_top = window_height - height / 2
-        var new_left = window_width - width / 2
+        var new_top = (window_height - height) / 2
+        var new_left = (window_width - width) / 2
         
         self.dom.main.css({'top': new_top + 'px', 'left': new_left + 'px'})    
     }
     
-    self.top_dialog = function() {
+    self.focus = function() {
         var zindexes = []
         $('.schnipp-dialog').each(function() {
             zindexes.push(parseInt($(this).css('z-index')))
@@ -75,6 +96,10 @@ schnipp.ui.dialog = function() {
         
         var new_index = Math.max.apply(null, zindexes) + 1
         self.dom.main.css('z-index', new_index)
+        self.events.fire('focus', {
+            evt: 'focus',
+            dialog: self
+        })
     }
 
     return self
