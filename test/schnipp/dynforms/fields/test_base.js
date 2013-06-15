9a93213e503c1c66642774424207fd5903d2911c
henzk <speidel@schnapptack.de>
2013-06-15 02:54:54 +0200

    fields.base: more tests

--- test/schnipp/dynforms/fields/test_base.js
+++ test/schnipp/dynforms/fields/test_base.js
@@ -9,11 +9,26 @@ test('render container of base field', function() {
         schema,
         ''
     )
-    equal(obj.dom.main, null)
+    equal(obj.dom.main, null, 'dom main is null')
     var input = $('<input type="text" name="test" value="">')
     var rendered = obj.render_container(schema, input)
     equal('test', rendered.find('input').attr('name'), 'rendered container contains input elem')
     ok(rendered.text().substr('Testlabel'), 'container contains label')
+})
+
+test('render container of base field without label', function() {
+    var schema = {
+        name: 'testname'
+    }
+    var obj = schnipp.dynforms.fields.base(
+        schema,
+        ''
+    )
+    equal(obj.dom.main, null, 'dom main is null')
+    var input = $('<input type="text" name="test" value="">')
+    var rendered = obj.render_container(schema, input)
+    equal('test', rendered.find('input').attr('name'), 'rendered container contains input elem')
+    ok(!rendered.find('label').length, 'container contains no label')
 })
 
 test('render view of base field', function() {
@@ -24,7 +39,7 @@ test('render view of base field', function() {
         },
         ''
     )
-    equal(obj.dom.main, null)
+    equal(obj.dom.main, null, 'dom main is null')
     obj.dom.input = $('<input type="text" name="test" value="">')
     var rendered = obj.render()
     obj.initialize()
@@ -32,6 +47,50 @@ test('render view of base field', function() {
     ok(rendered.text().substr('Testlabel'), 'view contains label')
 })
 
+test('initial: field with initial data', function() {
+    var obj = schnipp.dynforms.fields.base(
+        {
+            label: 'Testlabel',
+            name: 'testname'
+        },
+        'initial'
+    )
+    obj.dom.input = $('<input type="text" name="test" value="">')
+    var rendered = obj.render()
+    obj.initialize()
+    equal('initial', obj.get_initial_data(), 'explicit initial data is returned')
+})
+
+test('initial/default: field with no initial data, but default', function() {
+    var obj = schnipp.dynforms.fields.base(
+        {
+            label: 'Testlabel',
+            name: 'testname',
+            default_value: 'default'
+        }
+    )
+    obj.dom.input = $('<input type="text" name="test" value="">')
+    var rendered = obj.render()
+    obj.initialize()
+    equal('default', obj.get_initial_data(), 'get_initial: default_value is returned from schema')
+    equal('default', obj.get_default_data(), 'get_default: default_value is returned from schema')
+})
+
+test('initial/default: field with no initial data, no default', function() {
+    var obj = schnipp.dynforms.fields.base(
+        {
+            label: 'Testlabel',
+            name: 'testname',
+        }
+    )
+    /*patch in sentinel*/
+    obj.default_value = 'patched_default'
+    obj.dom.input = $('<input type="text" name="test" value="">')
+    var rendered = obj.render()
+    obj.initialize()
+    equal('patched_default', obj.get_initial_data(), 'get_initial: default_value is returned from field')
+    equal('patched_default', obj.get_default_data(), 'get_default: default_value is returned from field')
+})
 
 test('get, set, clear', function() {
     var obj = schnipp.dynforms.fields.base(
