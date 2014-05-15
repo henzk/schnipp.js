e5680cc96a0d08f20a094fac3b92773f7a3da915
tonimichel <toni@schnapptack.de>
2014-05-15 16:47:30 +0200

    added animated autoscrolling help text

--- src/schnipp/dynforms/visitors.js
+++ src/schnipp/dynforms/visitors.js
@@ -18,11 +18,27 @@ schnipp.dynforms.visitors.helptext_visitor = function() {
         
         field.events.bind('focus', function(args) {
             self.dom.display.empty()
-            if (args.help_text) {
-                self.dom.display.append('<i class="fa fa-info-circle"></i> ' + args.help_text)
-            } else if (field.field_descriptor.help_text) {
-                self.dom.display.append('<i class="fa fa-info-circle"></i> ' + field.field_descriptor.help_text)
-            }        
+            if (args.help_text || field.field_descriptor.help_text) {
+                if (args.help_text) {
+                    self.dom.display.append('<i class="fa fa-info-circle"></i> ' + args.help_text)
+                } else if (field.field_descriptor.help_text) {
+                    self.dom.display.append('<i class="fa fa-info-circle"></i> ' + field.field_descriptor.help_text)
+                }
+                
+                /* positioning the help text  */
+                var top = field.dom.main.position().top
+                if (top + self.dom.display.height() > form.dom.main.outerHeight()) {
+                    var value = top - self.dom.display.height() + field.dom.main.outerHeight()
+                } else {
+                    var value = top + parseInt(field.dom.main.css('padding-top').split('px')[0])
+                }
+                self.dom.display.css('opacity', '0.1')
+                self.dom.display.animate({
+                    'margin-top': value + 'px',
+                    'opacity': 1
+                })
+               
+            }
         })
         
         
