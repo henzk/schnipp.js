4f2c54b1e57e22f80b5823a87997b71d6acfb7bc
Toni Michel <toni.michel@schnapptack.de>
2016-01-13 11:04:12 +0100

    invoke set_data for setting initial data in intialize

--- src/schnipp/dynforms/abstract_field.js
+++ src/schnipp/dynforms/abstract_field.js
@@ -184,7 +184,11 @@ schnipp.dynforms.abstract_field = function(field_descriptor, field_data, parent_
         if ( field_descriptor.fetch_further ) {
             var root_form = self.get_root_form()
             $.each( field_descriptor.fetch_further, function(idx, fieldname) {
-                root_form.fields[fieldname].call_remote()
+                if ( root_form.fields[fieldname].call_remote ) {
+                    root_form.fields[fieldname].call_remote()
+                } else {
+                    console.error('Log: fetch_further field', fieldname, 'does not provide remote call functionality')
+                }
             } )
         }
     }
@@ -281,6 +285,7 @@ schnipp.dynforms.abstract_field = function(field_descriptor, field_data, parent_
      * @name schnipp.dynforms.abstract_field#initialize
      **/
     self.initialize = function() {
+        self.set_data(self.get_initial_data())
 
     }
 
